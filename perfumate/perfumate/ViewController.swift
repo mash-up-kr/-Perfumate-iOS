02198874db4bfe9ce3ed4317b52a8f803bf5eb75
JeongMinHo <wjdalsgh95@nate.com>
2021-09-27 00:42:55 +0900

    🎉: Init project

---
+++ perfumate/perfumate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  perfumate
+//
+//  Created by jeongminho on 2021/09/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
